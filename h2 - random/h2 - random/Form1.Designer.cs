f4d5663951bea86fd30bf29c63d3b3abdfcdb62f
GianlucaG00 <gianlu.gambi@gmail.com>
2022-12-01 23:52:50 +0100

    random

---
+++ h2 - random/h2 - random/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace h2___random
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Generate = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(181, 190);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(905, 390);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(181, 91);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(142, 52);
+            this.Generate.TabIndex = 1;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(407, 94);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(142, 49);
+            this.clearBtn.TabIndex = 2;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 676);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ h2 - random/h2 - random/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace h2___random
+{
+    public partial class Form1 : Form
+    {
+        double sum = 0;
+        int count = 0;
+        List<double> list = new List<double>(); 
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (timer1.Enabled == true)
+            {
+                Generate.Text = "Generate"; // change the text on the button
+                timer1.Enabled = false; 
+            }
+            else
+            {
+                Generate.Text = "Stop";
+                timer1.Enabled = true; 
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Random random = new Random();
+            double num = 0;
+            
+
+            num = random.NextDouble();
+            list.Add(num);  
+            
+
+            this.richTextBox1.AppendText(num.ToString() +  " ----> AVG: " + list.Average() + "\n"); 
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            sum = 0;
+            count = 0;
+            list.RemoveAll(x => true);
+        }
+    }
+}
